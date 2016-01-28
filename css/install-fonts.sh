540bdb1e4548f6788659b9a3ae9bb82639ff0d39
Maxim Lihachev <envrm@yandex.ru>
2016-01-28 09:53:52 +0500

    install-fonts.sh: изменение путей в файлах fontello

---
+++ css/install-fonts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Создан:  Чт 28 янв 2016 09:52:35
+#Изменён: Чт 28 янв 2016 09:52:35
+
+sed -i 's@../font/@../fonts/@g' fontello.css 
+sed -i 's@../font/@../fonts/@g' fontello-embedded.css
+
